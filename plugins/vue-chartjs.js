a57667e6682c5afc7ce8bcff8d830096273ebf2f
xamuel98 <samuelakintomiwa98@gmail.com>
2023-10-08 11:52:06 +0100

    feat: Integrate nuxtjs 2 with vue-chartjs and chartjs

---
+++ plugins/vue-chartjs.js
@@ -0,0 +1,3 @@
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
